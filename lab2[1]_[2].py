fe4efa55bfaf5023337b454ac796970128c9f96a
София Тараненко <taransofa44@gmail.com>
2023-03-05 14:50:40 +0700

    Раздел 1- Задание 2

---
+++ lab2[1]_[2].py
@@ -0,0 +1,12 @@
+#Задание 1.2
+print("Введите три числа:")
+x = int(input())
+y = int(input())
+z = int(input())
+
+if x >= 1 and x <= 50:
+    print(x)
+if y >= 1 and y <= 50:
+    print(y)
+if z >= 1 and z <= 50:
+    print(z)
